fb5dbf9ccaff49f92400b3e31775495d3bd03425
Abed Murad <Abdallah.Murad@ProtonMail.com>
2020-05-26 20:22:54 +0300

    Activate Sales on Admin Side.

--- app/src/main/java/com/example/store/adapters/SalesAdapter.kt
+++ app/src/main/java/com/example/store/adapters/SalesAdapter.kt
@@ -6,14 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.store.R
+import com.example.store.model.Category
 import com.example.store.model.Sale
 import kotlinx.android.synthetic.main.sale_item_layout.view.*
 
-class SalesAdapter(var context: Context?, var data:List<Sale>):RecyclerView.Adapter<SalesAdapter.SalesViewHolder>() {
+class SalesAdapter(var context: Context?, var data: List<Sale>) :
+    RecyclerView.Adapter<SalesAdapter.SalesViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SalesViewHolder {
-        return SalesViewHolder(LayoutInflater.from(context).inflate(R.layout.sale_item_layout,parent,false))
+        return SalesViewHolder(LayoutInflater.from(context).inflate(R.layout.sale_item_layout, parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -21,12 +23,14 @@ class SalesAdapter(var context: Context?, var data:List<Sale>):RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: SalesViewHolder, position: Int) {
-        holder.tvCustomerName.text=data[position].buyerEmail
-        holder.tv_product_sale_name.text=data[position].buyerEmail
+        holder.tvCustomerName.text = "Product Name: ${data[position].ProductName}"
+        holder.tv_product_sale_name.text = "Customer Name: ${data[position].username} "
+        holder.productDetails.text = "Product details: ${data[position].productRef} "
     }
-    class SalesViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
+
+    class SalesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var tv_product_sale_name = itemView.tv_product_sale_name
         var tvCustomerName = itemView.tvCustomerName
-
+        var productDetails = itemView.productDetails
     }
 }
--- app/src/main/java/com/example/store/model/Sale.kt
+++ app/src/main/java/com/example/store/model/Sale.kt
@@ -1,4 +1,9 @@
 package com.example.store.model
 
-class Sale(var buyerEmail: String= "", var buyerName: String= "", var itemName: String = "") {
+class Sale(
+    var userId: String = "",
+    var ProductName: String = "",
+    var productRef: String = "",
+    var username: String = ""
+) {
 }
