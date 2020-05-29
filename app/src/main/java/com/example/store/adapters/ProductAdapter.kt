88af33fd472091aee9764058cc4e00668ed94e62
Abed Murad <Abdallah.Murad@ProtonMail.com>
2020-05-29 20:08:28 +0300

    update product image.

--- app/src/main/java/com/example/store/adapters/ProductAdapter.kt
+++ app/src/main/java/com/example/store/adapters/ProductAdapter.kt
@@ -5,14 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.store.R
 import com.example.store.adapters.HomeAdapter.OnProductClickListener
 import com.example.store.model.Brand
 import com.example.store.model.Product
+import kotlinx.android.synthetic.main.activity_second.imgAboutItem
 import kotlinx.android.synthetic.main.brand_layout.view.*
 import kotlinx.android.synthetic.main.item_product.view.name
+import java.lang.System.load
 
-class ProductAdapter(var context: Context, var products: List<Product> , var onProductClickListener: com.example.store.adapters.HomeAdapter.OnProductClickListener) :
+class ProductAdapter(
+    var context: Context,
+    var products: List<Product>,
+    var onProductClickListener: com.example.store.adapters.HomeAdapter.OnProductClickListener
+) :
     RecyclerView.Adapter<ProductAdapter.BrandViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BrandViewHolder {
@@ -25,24 +32,19 @@ class ProductAdapter(var context: Context, var products: List<Product> , var onP
 
     override fun onBindViewHolder(holder: BrandViewHolder, position: Int) {
         holder.rbBrand.name.text = products[position].name.toString()
-
+        Glide.with(holder.itemView.context).load(products[position].img).into(holder.img)
 
         holder.itemView.setOnClickListener {
             onProductClickListener.onItemClicked(products[position].id)
         }
-
-
     }
 
     class BrandViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var rbBrand = itemView.name
+        var img = itemView.imgBrand
     }
 
-
-    interface OnProductClickListener{
-        fun onItemClicked(id:String);
+    interface OnProductClickListener {
+        fun onItemClicked(id: String);
     }
-
-
-
 }
